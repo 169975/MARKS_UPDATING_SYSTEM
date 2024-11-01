7471de892409b7311ec4caed161469e921a21156
GitongaDaisyGatugi <146339788+169975@users.noreply.github.com>
2024-11-01 12:19:37 +0300

    I have modified the classes

---
+++ objects.py
@@ -0,0 +1,8 @@
+from person import Person
+
+student = Person("John", "Student")
+print(student.do_exams())
+
+lecturer = Person("Paul", "Lecturer")
+print(lecturer.do_exams())
+
--- person.py
+++ person.py
@@ -3,25 +3,15 @@ class Person:
         self.name = name
         self.role = role
 
-class Student(Person):
-    def __init__(self, name, role):
-        super().__init__(name, role)
 
-    def do_exams(self):
-        print(f"{self.name}")
-        return 0
-    
-Student= Person("John","student")
-print(Student.do_exams())
-
-
-class Lecturer(Person):
-    def __init__(self, name, role):
-        super().__init__(name, role)
-
-    def update_marks(self):
+def do_exams(self):
         print(f"{self.name}")
         return 0
 
-Lecturer= Person("Paul","Lecturer")
-print(Lecturer.update_marks())
+
+def update_marks(self):
+        print(f"{self.name}")
+        return 0
+
+
+
